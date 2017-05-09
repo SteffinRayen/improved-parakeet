b3a046ff34883ada1360d75ff62ab1fb422de87e
Steffin Rayen <SteffinRayen@users.noreply.github.com>
2017-05-09 17:42:24 +0530

    Silly update (/.\)

--- ImageDistortionUsingCoordinates.py
+++ ImageDistortionUsingCoordinates.py
@@ -6,14 +6,21 @@ from PIL import Image
 
 img = cv2.imread(str(sys.argv[1]))
 rows,cols,ch = img.shape
-newCoordinate = [
-					[int(sys.argv[2]),int(sys.argv[3])],
-					[int(sys.argv[4]),int(sys.argv[5])],
-					[int(sys.argv[6]),int(sys.argv[7])],
-					[int(sys.argv[8]),int(sys.argv[9])]
-				]
+oldCoordinate = [
+			[int(sys.argv[2]),int(sys.argv[3])],
+			[int(sys.argv[4]),int(sys.argv[5])],
+			[int(sys.argv[6]),int(sys.argv[7])],
+			[int(sys.argv[8]),int(sys.argv[9])]
+		]
 
-pts1 = np.float32([[141,276],[503,276],[503,417],[141,417]])
+newCoordinate = [
+			[int(sys.argv[10]),int(sys.argv[11])],
+			[int(sys.argv[12]),int(sys.argv[13])],
+			[int(sys.argv[14]),int(sys.argv[14])],
+			[int(sys.argv[16]),int(sys.argv[15])]
+		]
+
+pts1 = np.float32(oldCoordinate)
 pts2 = np.float32(newCoordinate)
 
 M = cv2.getPerspectiveTransform(pts1,pts2)
